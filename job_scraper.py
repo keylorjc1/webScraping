7eacd211b892c9c72920a89288183c727ca1ca18
keylorjc1 <106986264+keylorjc1@users.noreply.github.com>
2025-08-08 11:18:05 -0600

    Updates

---
+++ job_scraper.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+
+soup = BeautifulSoup(html_text, 'lxml')
+
+jobs = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = jobs.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+print(company_name)
+jobs_skills = soup.find('div', class_ = 'srp-skills').text.replace(' ','')
+print(jobs_skills)
